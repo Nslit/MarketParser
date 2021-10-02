a358bff0a626a7caba228241b629a3cbd3340dbd
Nslit <79807168+Nslit@users.noreply.github.com>
2021-10-02 19:48:14 +0300

    Nslit
    создание ссылок. соупов

---
+++ parser.py
@@ -0,0 +1,28 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+
+ozon_sort_list = ('score', 'new', 'price', 'rating', 'discount')
+sber_sort_list = (0, 4, 1, 3, 6)
+wild_sort_list = ('popular', 'newly', 'priceup', 'rate', 'sale')
+
+
+def ozon_search(product, sorter=0):
+    return f"https://www.ozon.ru/search/?from_global=true&sorting={ozon_sort_list[sorter]}&text={quote(product)}"
+
+
+def sber_search(product, sorter=0):
+    return f'https://sbermegamarket.ru/catalog/?q={quote(product)}#?sort={sber_sort_list[sorter]}'
+
+
+def wild_search(product, sorter=0):
+    return f'https://www.wildberries.ru/catalog/0/search.aspx?page=1&sort={wild_sort_list[sorter]}&search={quote(product)}'
+
+
+def soup_creation(url):
+    html = urlopen(url).read().decode('utf-8')  # Получение html по url
+    soup = BeautifulSoup(html, 'html.parser')  # Создание соупа из html
+    return soup
+
+
+print(soup_creation(ozon_search('губка')))
